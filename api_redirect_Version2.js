241536bbb9015abe82e9bf417bb6400f3be9326c
henrylin1120 <lchhenry2004@gmail.com>
2025-08-07 11:05:43 +0800

    Initial commit: Add API redirect functionality

---
+++ api_redirect_Version2.js
@@ -0,0 +1,17 @@
+export default function handler(request) {
+  const userAgent = request.headers.get('user-agent') || '';
+  const url = new URL(request.url);
+  
+  // Detect iOS devices
+  if (/iPad|iPhone|iPod/.test(userAgent)) {
+    return Response.redirect('https://apps.apple.com/us/app/candy-crush-saga/id553834731', 302);
+  }
+  
+  // Detect Android devices
+  if (/Android/.test(userAgent)) {
+    return Response.redirect('https://play.google.com/store/apps/details?id=com.king.candycrushsaga', 302);
+  }
+  
+  // Default to web version for desktop/other devices
+  return Response.redirect('https://your-web-app.com', 302);
+}
